af6c0277c1fa10bdcf5b837469fc617dbe05dc07
Jamie Matthews <jamie@jmatthews.uk>
2024-07-17 02:27:01 +0100

    * Redesigned the form. * Added a "Delete Flies" option. * Added drag and drop functionality.

--- sep-micro/MainWindow.xaml.cs
+++ sep-micro/MainWindow.xaml.cs
@@ -1,15 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Input;
-using Microsoft.Win32;
 
 namespace sep_micro
 {
@@ -26,7 +17,7 @@ namespace sep_micro
 
         string[] selectedFilePaths;
         string[] selectedFileNames;
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btnSelectFiles_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Multiselect = true;
@@ -35,82 +26,67 @@ namespace sep_micro
                 selectedFilePaths = ofd.FileNames;
                 selectedFileNames = ofd.SafeFileNames;
 
-                //If any of the selected files end with the extension '.aes', then all of the selected files need to. If they don't give an error
-                foreach (string file in selectedFileNames)
-                {
-                    if (file.EndsWith(".aes"))
-                    {
-                        bool allEncrypted = true;
-                        foreach(string subfile in selectedFileNames)
-                        {
-                            if(!subfile.EndsWith(".aes"))
-                                allEncrypted = false;
-                        }
-                        if (allEncrypted)
-                            encryptMode = false;
-                        else
-                        {
-                            MessageBox.Show("You have some files that are already encrypted!\r\nPlease ensure that either all the selected files have the extension of '.aes', or none of them do.");
-                            selectedFileNames = null;
-                            selectedFilePaths = null;
-                            return;
-                        }
-                            
-                    }
-                }
-
-                if (selectedFilePaths.Length == 0)
-                {
-                    MessageBox.Show("No files selected");
-                }
-                else if (selectedFilePaths.Length==1)
-                {
-                    lbFileDisplayPreview.Content = selectedFileNames[0];
-                }
-                else
-                {
-                    lbFileDisplayPreview.Content = selectedFilePaths.Length.ToString() + " files selected";
-                }
-
-                if(!encryptMode)
-                {
-                    btnFunction.Content = "Decrypt File";
-                }
-                else
-                {
-                    btnFunction.Content = "Encrypt File";
-                }
+                FileHandler();
             }
         }
 
-        private void pbShowPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        private void FileHandler()
         {
-            pbShowPassword.Source = new BitmapImage(new Uri("pack://application:,,,/password_reveal-active.png"));
-        }
+            //If any of the selected files end with the extension '.aes', then all of the selected files need to. If they don't give an error
+            foreach (string file in selectedFileNames)
+            {
+                if (file.EndsWith(".aes"))
+                {
+                    bool allEncrypted = true;
+                    foreach (string subfile in selectedFileNames)
+                    {
+                        if (!subfile.EndsWith(".aes"))
+                            allEncrypted = false;
+                    }
+                    if (allEncrypted)
+                        encryptMode = false;
+                    else
+                    {
+                        MessageBox.Show("You have some files that are already encrypted!\r\nPlease ensure that either all the selected files have the extension of '.aes', or none of them do.");
+                        selectedFileNames = null;
+                        selectedFilePaths = null;
+                        lbFileDisplayPreview.Content = "Drag and drop files here";
+                        return;
+                    }
 
-        private void pbShowPassword_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            pbShowPassword.Source = new BitmapImage(new Uri("pack://application:,,,/password_reveal-default.png"));
-        }
+                }
+            }
 
-        private void pbCopy_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            pbCopy.Source = new BitmapImage(new Uri("pack://application:,,,/copy-icon_active.jpg"));
-            Clipboard.SetText(pwSecretKey.Password);
-        }
+            if (selectedFilePaths.Length == 0)
+            {
+                MessageBox.Show("Drag and drop files here");
+            }
+            else if (selectedFilePaths.Length == 1)
+            {
+                lbFileDisplayPreview.Content = selectedFileNames[0];
+            }
+            else
+            {
+                lbFileDisplayPreview.Content = selectedFilePaths.Length.ToString() + " files selected";
+            }
 
-        private void pbCopy_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            pbCopy.Source = new BitmapImage(new Uri("pack://application:,,,/copy-icon.jpg"));
+            if (!encryptMode)
+            {
+                btnFunction.Content = "Decrypt File";
+            }
+            else
+            {
+                btnFunction.Content = "Encrypt File";
+            }
         }
 
         private void btnFunction_Click(object sender, RoutedEventArgs e)
         {
-            if(pwSecretKey.Password == null || pwSecretKey.Password=="")
+            if (pwSecretKey.Password == null || pwSecretKey.Password == "")
             {
                 MessageBox.Show("You must enter a password.");
             }
-            else if(selectedFilePaths==null || selectedFilePaths.Length<1)
+            else if (selectedFilePaths == null || selectedFilePaths.Length < 1)
             {
                 MessageBox.Show("You must select at least 1 file to encrypt or decrypt.");
             }
@@ -124,11 +100,6 @@ namespace sep_micro
                         AES.EncryptFile(file, file + ".aes", pwSecretKey.Password);
                     }
                     MessageBox.Show($"{selectedFilePaths.Length} files encrypted.", "SEP");
-                    pwSecretKey.IsEnabled = true;
-                    pwSecretKey.Password = "";
-                    selectedFileNames = null;
-                    selectedFilePaths = null;
-                    lbFileDisplayPreview.Content = "";
                 }
                 else
                 {
@@ -137,13 +108,107 @@ namespace sep_micro
                         AES.DecryptFile(file, file.Substring(0, file.Length - 4), pwSecretKey.Password);
                     }
                     MessageBox.Show($"{selectedFilePaths.Length} files decrypted.", "SEP");
-                    pwSecretKey.IsEnabled = true;
-                    pwSecretKey.Password = "";
-                    selectedFileNames = null;
-                    selectedFilePaths = null;
-                    lbFileDisplayPreview.Content = "";
+                }
+                if (cbDeleteFiles.IsChecked == true)
+                {
+                    foreach (string file in selectedFilePaths)
+                    {
+                        OtherOperations.WipeFile(file, 3);
+                    }
+                }
+                pwSecretKey.IsEnabled = true;
+                pwSecretKey.Password = "";
+                lbFileDisplayPreview.Content = "Drag and drop files here";
+                selectedFileNames = null;
+                selectedFilePaths = null;
+            }
+        }
+
+        private void btnClearFiles_Click(object sender, RoutedEventArgs e)
+        {
+            selectedFileNames = null;
+            selectedFilePaths = null;
+            lbFileDisplayPreview.Content = "Drag and drop files here";
+            btnFunction.Content = "Ready";
+        }
+
+        private void btnShowPassword_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(pwSecretKey.Password);
+        }
+
+        private void ShowPasswordFunction()
+        {
+            pwVisible.Visibility = Visibility.Visible;
+            pwSecretKey.Visibility = Visibility.Hidden;
+            pwVisible.Text = pwSecretKey.Password;
+        }
+
+        private void HidePasswordFunction()
+        {
+            pwVisible.Visibility = Visibility.Hidden;
+            pwSecretKey.Visibility = Visibility.Visible;
+            pwVisible.Text = "";
+        }
+
+        private void btnShowPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ShowPasswordFunction();
+        }
+
+        private void btnShowPassword_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            HidePasswordFunction();
+        }
+
+        private void DropZone_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void DropZone_DragLeave(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void DropZone_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                selectedFilePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+                //Get safe file names
+                selectedFileNames = new string[selectedFilePaths.Length];
+                for (int i = 0; i < selectedFilePaths.Length; i++)
+                {
+                    selectedFileNames[i] = System.IO.Path.GetFileName(selectedFilePaths[i]);
+                }
+                if (selectedFilePaths.Length == 0)
+                {
+                    MessageBox.Show("Drag and drop files here");
+                }
+                else if (selectedFilePaths.Length == 1)
+                {
+                    lbFileDisplayPreview.Content = selectedFileNames[0];
+                }
+                else
+                {
+                    lbFileDisplayPreview.Content = selectedFilePaths.Length.ToString() + " files selected";
                 }
             }
+
+            FileHandler();
         }
     }
 }
---
+++ sep-micro/OtherOperations.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace sep_micro
+{
+    class OtherOperations
+    {
+        public static void WipeFile(string file, int passes)
+        {
+            try
+            {
+                if (File.Exists(file))
+                {
+                    // Set the file's attributes to normal in case it's read-only.
+                    File.SetAttributes(file, FileAttributes.Normal);
+
+                    // Calculate the total number of sectors in the file.
+                    double sectors = Math.Ceiling(new FileInfo(file).Length / 512.0);
+
+                    // Create a dummy buffer the same size as the sector.
+                    byte[] dummyBuffer = new byte[512];
+
+                    // Create a cryptographic Random Number Generator.
+                    // This is what we'll be filling the file with.
+                    RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+                    // Open a FileStream to the file.
+                    FileStream inputStream = new FileStream(file, FileMode.Open);
+                    for (int currentPass = 0; currentPass < passes; currentPass++)
+                    {
+                        // Go to the beginning of the stream
+                        inputStream.Position = 0;
+
+                        // Loop all sectors
+                        for (int sectorsWritten = 0; sectorsWritten < sectors; sectorsWritten++)
+                        {
+                            // Fill the dummy buffer with random data
+                            rng.GetBytes(dummyBuffer);
+
+                            // Write it to the stream
+                            inputStream.Write(dummyBuffer, 0, dummyBuffer.Length);
+                        }
+                    }
+
+                    // Close the stream.
+                    inputStream.Close();
+
+                    // As an extra measure, let's calculate a random filename to rename our file to, before deleting it.
+                    string newName = Path.GetDirectoryName(file) + "\\" + Path.GetRandomFileName();
+                    File.Move(file, newName);
+
+                    File.Delete(newName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: {0}", ex.Message);
+                MessageBox.Show($"Sorry, these files could not be deleted. Please try again.\r\nMore Details: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
